b5a0303ddfd3b00f6f5a350dc163ead2745fb248
copriwolf <CopriwolfdeMacBook-Pro.local>
2018-01-05 21:46:59 +0800

    change pa

--- config/db.php
+++ config/db.php
@@ -4,7 +4,7 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=questionSurvey',
     'username' => 'root',
-    'password' => '19860311',
+    'password' => 'xxxxxxxxx',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
